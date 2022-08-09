d840712a5dcb1b4ced8d338bcda5da2f7943fd68
Farhan Alam <59445081+Farhan5897@users.noreply.github.com>
2022-08-10 00:07:59 +0600

    ui updated added onclick listener on button

--- app/src/main/java/com/simatrix/logbook/MainActivity.kt
+++ app/src/main/java/com/simatrix/logbook/MainActivity.kt
@@ -2,10 +2,17 @@ package com.simatrix.logbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var add: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        add = findViewById(R.id.add_button)
+        add.setOnClickListener {
+            Toast.makeText(this, "moga", Toast.LENGTH_SHORT).show()
+        }
     }
 }
